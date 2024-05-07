32e989926feea3431a557eb01098df36ec989c3a
Yifaht Meir <yifahtmyers@gmail.com>
2024-05-07 10:56:35 +0300

    Connect mongodb

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
-    dbName: 'Instaframe',
+    dbURL: 'mongodb+srv://yifaht:2S2s2s2s2s2s2s@cluster0.crpigkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbName: 'Instaframe_db',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbName: 'Instaframe',
+    dbURL: 'mongodb+srv://yifaht:2S2s2s2s2s2s2s@cluster0.crpigkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbName: 'Instaframe_db',
 }
